1b9bb95e51ecc3f9817c7ae7d7e114c363e898fd
tdreams <fabriels@gmail.com>
2023-06-09 16:53:40 -0700

    conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Stakepool-Frontend/",
   plugins: [react()],
-})
+  esolve: {
+    alias: {
+      "~": path.resolve(__dirname, "node_modules"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
