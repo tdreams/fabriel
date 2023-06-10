81972e23f55a358a68ea397015fa597a6fa7decf
tdreams <fabriels@gmail.com>
2023-06-09 17:01:57 -0700

    conf

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Stakepool-Frontend/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: "react/dist/react.esm-bundler.js",
-      ziggy: path.resolve("vendor/tightenco/ziggy/dist/react.es.js"),
-    },
-  },
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+        manualChunks: (id) => {
+          if (id.includes("node_modules") && id.endsWith(".js")) {
+            return "vendor";
           }
         },
       },
     },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as per your needs
   },
 });
