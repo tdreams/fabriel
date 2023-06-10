90993c377e732db92b36f579793aae8b91ba9d87
tdreams <fabriels@gmail.com>
2023-06-09 17:05:21 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as per your needs
+    chunkSizeWarningLimit: 5000, // Adjust the chunk size warning limit as per your needs
   },
 });
